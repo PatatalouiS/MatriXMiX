34eae16a1d77c9de73e4bfc3040c3386b396d47f
hana.sebia <hana.sebia@etu.univ-lyon1.fr>
2019-03-27 22:29:35 +0100

    suivi de réparation

---
+++ src/MatriXMiXTXT.h
@@ -0,0 +1,43 @@
+
+#ifndef _MATRIXMIX_TXT_H_
+#define _MATRIXMIX_TXT_H_
+
+#include "MatrixLibrary.h"
+
+
+class MatriXMiXTXT
+{
+    private:
+    
+    MatrixLibrary lib;
+	
+	
+	void binaryOperation (const char op) const;
+	void binaryOperationPow() const;
+	void unaryOperation(const char op) const;
+    void MsgEmptyLib () const;
+	
+	
+    void addMatrixMenu ();
+    void showLibrary () const;
+    void addition () const;
+	void soustraction () const;
+	void multiplication () const;
+	void division () const;
+	void puissance () const;
+	void trace() const;
+	void determinant() const;
+	void inverse() const;
+    
+    
+
+    public:
+    
+    void mainMenu ();
+    
+    
+};
+
+
+
+#endif
---
+++ src/MatrixLibrary.h
@@ -0,0 +1,30 @@
+
+#ifndef _MATRIX_LIBRARY_
+#define _MATRIX_LIBRARY_
+
+#include "Matrix.h"
+#include <map>
+
+
+class MatrixLibrary
+{
+	private:
+	
+	std::map<std::string, Matrix> tab;
+	
+	public:
+	
+    MatrixLibrary ();
+    ~MatrixLibrary ();
+    
+    unsigned int long size () const;
+    bool isEmpty () const;
+	bool exist (const std::string& name) const;
+    void print () const;
+	void addMatrix (const std::string& name, const Matrix& m);
+	const Matrix* find (const std::string& name) const;
+	void erase (const std:: string & name);
+};
+
+
+#endif
