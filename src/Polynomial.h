e52799a67da2af6385db3751a390a1d2dc58d8ea
hana.sebia <hana.sebia@etu.univ-lyon1.fr>
2019-04-06 11:53:34 +0200

    Commit de merge pour eigen

--- src/Polynomial.h
+++ src/Polynomial.h
@@ -28,8 +28,7 @@ public:
     const Polynomial operator -(const Polynomial & p);
     const Polynomial operator *(const Polynomial & p);
     const Polynomial operator *(const float & scale);
-    const Polynomial division (const Polynomial & divisor,Polynomial & reste);
-    void equation2degre (unsigned int & nbsolution, double & x1, double & x2);
+
 
 
 };
