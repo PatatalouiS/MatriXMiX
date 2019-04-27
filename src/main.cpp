1c25d4ecbe002d69cae1e3c4e5d6c68ba34e1ab6
OLIVIE MAXIME p1710179 <maxime.olivie@etu.univ-lyon1.fr>
2019-04-27 12:44:55 +0200

    nouveau main simpliste

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,7 @@
 int main (int argc, char* argv[])
 {
     QApplication app(argc, argv);
-<<<<<<< HEAD
     MainWindow win;
     win.show();
-=======
-    app.setQuitOnLastWindowClosed(false);
-    MainWindow* win = new MainWindow;
-    win->show();
->>>>>>> origin/version_graph_ana
     return app.exec();
 }
-
