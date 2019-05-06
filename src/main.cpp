e40ee3675fa5f7a012f9bd23b319a873445d5437
OLIVIE MAXIME p1710179 <maxime.olivie@etu.univ-lyon1.fr>
2019-05-06 02:12:01 +0200

    main de base

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@
 
 
 int main (int argc, char* argv[])
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/calcul_matriciel_tarik
 {
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/img/icons/MatriXMiX.png"));
